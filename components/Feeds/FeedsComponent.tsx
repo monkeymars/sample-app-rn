339a7f3db111320d14ed867c718bc7d97fa74741
Gery Wibowo <gery.wibowo@lemonilo.com>
2023-05-28 14:33:08 +0700

    rnw: app

---
+++ components/Feeds/FeedsComponent.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const FeedsComponent = () => {
+  return (
+    <View className="relative overflow-hidden rounded-lg bg-white px-4 pb-12 pt-5 shadow sm:px-6 sm:pt-6">
+      <Text>Lemonilo Feeds</Text>
+    </View>
+  );
+};
+
+export default FeedsComponent;
